abfd4e1c9c32357e21a7c83d9bd93ae191cabb8f
Tommy <SUP3RS4IY4NX@gmail.com>
2025-11-10 12:26:33 -0500

    Fixed minor bug

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,7 @@
 import { HeadContent, Scripts, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanStackDevtools } from '@tanstack/react-devtools'
-import { getUser } from '@/lib/serverFunctions/getUserFn'
+import { getUserFn } from '@/lib/serverFunctions/getUserFn'
 import { UserProvider } from "@/context/UserContext";
 import Header from '../components/Header'
 
@@ -29,7 +29,7 @@ export const Route = createRootRoute({
     ],
   }),
   loader: async () => {
-    const user = await getUser()
+    const user = await getUserFn()
     return { user }
   },
   shellComponent: RootDocument,
